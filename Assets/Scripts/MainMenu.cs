2002fc7adeaa2c00dab277f6d6dd00f7b9f3105b
Joe <jxi8@pct.edu>
2025-04-30 10:54:00 -0400

    Final Final Final (Added Info Tab)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public GameObject mainMenu;
     public GameObject levelSelect;
     public GameObject settingsMenu;
+    public GameObject infoMenu;
     public SoundManager soundManager;
 
     public void LevelSelect()
@@ -17,6 +18,7 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(false);
         levelSelect.SetActive(true);
         settingsMenu.SetActive(false);
+        infoMenu.SetActive(false);
     }
     public void SettingsMenu()
     {
@@ -25,6 +27,16 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(false);
         levelSelect.SetActive(false);
         settingsMenu.SetActive(true);
+        infoMenu.SetActive(false);
+    }
+    public void InfoMenu()
+    {
+        //Opens Info menu and closes other ones
+        soundManager.TowerSelect();
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(false);
+        settingsMenu.SetActive(false);
+        infoMenu.SetActive(true);
     }
     public void BackToMainMenu()
     {
@@ -33,6 +45,7 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(true);
         levelSelect.SetActive(false);
         settingsMenu.SetActive(false);
+        infoMenu.SetActive(false);
     }
     public void Quit()
     {
